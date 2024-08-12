16efd055c80f70754597b134d2ff8ac28408bf47
Chris Norulak <midnitte@gmail.com>
2024-08-12 13:00:16 -0400

    GC content problem fixed

--- Computing GC Content/gc_content.py
+++ Computing GC Content/gc_content.py
@@ -5,9 +5,14 @@ from pathlib import Path
 
 file_path = Path(__file__).parent / "rosalind_gc.txt"
 
+result = None
+highest_gc = 0.00001
 
 for record in SeqIO.parse(file_path, "fasta"):
-    print(f"> {record.id}\n{gc_fraction(record)*100:.6f}")
+    if float(gc_fraction(record)*100.0) > float(highest_gc):
+        highest_gc = gc_fraction(record)*100.000000
+        result = f"{record.id}\n{gc_fraction(record)*100:.6f}"
+print(result)
 
 
 
